a17652fd78a5850c2c1e2f823e33c9d62495b30b
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-27 10:13:37 +0000

    added get method to retrieve the next question

--- GeekQuiz/Controllers/TriviaController.cs
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using GeekQuiz.Models;
 
 namespace GeekQuiz.Controllers
@@ -19,6 +22,37 @@ namespace GeekQuiz.Controllers
                 this.db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+        {
+            var lastQuestionId = await this.db.TriviaAnswers
+                .Where(a => a.UserId == userId)
+                .GroupBy(a => a.QuestionId)
+                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+                .OrderByDescending(q => new {q.Count, QuestionId = q.QuestionId})
+                .Select(q => q.QuestionId)
+                .FirstOrDefaultAsync();
+
+            var questionsCount = await this.db.TriviaQuestions.CountAsync();
+
+            var nextQuestionId = (lastQuestionId%questionsCount) + 1;
+            return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
+        }
+
+        // Get api/Trivia
+        [ResponseType(typeof (TriviaQuestion))]
+        public async Task<IHttpActionResult> Get()
+        {
+            var userId = User.Identity.Name;
+
+            TriviaQuestion nextQuestion = await this.NextQuestionAsync(userId);
+
+            if (nextQuestion == null)
+            {
+                return this.NotFound();
+            }
+            return this.Ok(nextQuestion);
         }
     }
 }
