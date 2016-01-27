311a8085170031e56ca757321c622ed1577e278c
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-27 09:54:57 +0000

    added json formatting and trivia controller

---
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GeekQuiz.Models;
+
+namespace GeekQuiz.Controllers
+{
+    public class TriviaController : ApiController
+    {
+        private TriviaContext db = new TriviaContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
